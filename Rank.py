587788eb0e9e86c5f2436243547ec3f04807380a
hemanshidolar166-alt <hemanshidolar166@gmail.com>
2026-08-13 22:08:51 +0530

    Add files via upload
    Student Rank Processing Engine
    Develop a Python application that reads student records from User input.
    Each record contains:
    Roll No
    Name
    Marks in 5 subjects
    The program must:
    Compute total and percentage.
    Assign grades.
    Handle ties while assigning ranks (students with equal marks receive the same rank).
    Display the next rank correctly (e.g., 1,2,2,4).

---
+++ Rank.py
@@ -0,0 +1,53 @@
+n = int(input("Enter number of students: "))
+roll = []
+name = []
+total = []
+percentage = []
+grade = []
+rank = []
+
+for i in range(n):
+    print("\nEnter details of Student", i + 1)
+    r = int(input("Enter Roll No: "))
+    nm = input("Enter Name: ")
+    
+    m1 = float(input("Enter marks of Subject 1: "))
+    m2 = float(input("Enter marks of Subject 2: "))
+    m3 = float(input("Enter marks of Subject 3: "))
+    m4 = float(input("Enter marks of Subject 4: "))
+    m5 = float(input("Enter marks of Subject 5: "))
+
+    t = m1 + m2 + m3 + m4 + m5
+    p = t / 5
+
+    if p >= 90:
+        g = "A+"
+    elif p >= 80:
+        g = "A"
+    elif p >= 70:
+        g = "B"
+    elif p >= 60:
+        g = "C"
+    elif p >= 50:
+        g = "D"
+    else:
+        g = "F"
+    roll.append(r)
+    name.append(nm)
+    total.append(t)
+    percentage.append(p)
+    grade.append(g)
+
+# Rank
+for i in range(n):
+    rnk = 1
+    for j in range(n):
+        if total[j] > total[i]:
+            rnk = rnk + 1
+    rank.append(rnk)
+# Display
+print("\nRollNo\tName\tTotal\tPercentage\tGrade\tRank")
+for i in range(n):
+
+    print(roll[i], "\t", name[i], "\t", total[i],
+          "\t", percentage[i], "%\t\t", grade[i], "\t", rank[i])
