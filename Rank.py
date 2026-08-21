4cd127055e490164e3e80b34c2457c404e661496
hemanshidolar166-alt <hemanshidolar166@gmail.com>
2026-08-21 12:42:58 +0530

    Rank.py

--- Rank.py
+++ Rank.py
@@ -1,4 +1,5 @@
 n = int(input("Enter number of students: "))
+
 roll = []
 name = []
 total = []
@@ -8,9 +9,10 @@ rank = []
 
 for i in range(n):
     print("\nEnter details of Student", i + 1)
+
     r = int(input("Enter Roll No: "))
     nm = input("Enter Name: ")
-    
+
     m1 = float(input("Enter marks of Subject 1: "))
     m2 = float(input("Enter marks of Subject 2: "))
     m3 = float(input("Enter marks of Subject 3: "))
@@ -32,22 +34,40 @@ for i in range(n):
         g = "D"
     else:
         g = "F"
+
     roll.append(r)
     name.append(nm)
     total.append(t)
     percentage.append(p)
     grade.append(g)
 
+
+# Sort in descending order of total
+for i in range(n):
+    for j in range(i + 1, n):
+        if total[j] > total[i]:
+
+            total[i], total[j] = total[j], total[i]
+            roll[i], roll[j] = roll[j], roll[i]
+            name[i], name[j] = name[j], name[i]
+            percentage[i], percentage[j] = percentage[j], percentage[i]
+            grade[i], grade[j] = grade[j], grade[i]
+
+
 # Rank
 for i in range(n):
     rnk = 1
+
     for j in range(n):
         if total[j] > total[i]:
             rnk = rnk + 1
+
     rank.append(rnk)
+
+
 # Display
 print("\nRollNo\tName\tTotal\tPercentage\tGrade\tRank")
-for i in range(n):
 
+for i in range(n):
     print(roll[i], "\t", name[i], "\t", total[i],
           "\t", percentage[i], "%\t\t", grade[i], "\t", rank[i])
